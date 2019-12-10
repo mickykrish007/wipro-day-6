38a97ca94d5f8b761b8a4b31cc3146337fd05603
mickykrish007 <58469412+mickykrish007@users.noreply.github.com>
2019-12-10 16:37:01 +0530

    Add files via upload

---
+++ TestEmployee.java
@@ -0,0 +1,67 @@
+public class TestEmployee {
+    public static void main(String[] args) {
+        
+        Teacher  t1  = new Teacher();
+        CollegeStudent cs = new CollegeStudent();
+        t1.setnameOfPerson("Saikiran");
+        t1.setDetails(10000, "Java");
+        cs.setNameOfStudent("Jaddu");
+        cs.setDetails(2, "CSE");
+        t1.display();
+        cs.display();
+        
+    }
+    
+}
+class Person
+{
+    String nameOfPerson;
+    void setnameOfPerson(String s)
+    {
+        nameOfPerson = s ;
+    }
+    
+}
+
+class Student extends Person
+{
+    String nameOfStudent;
+    void setNameOfStudent(String s)
+    {
+        nameOfStudent = s;
+    }
+    
+}
+class Teacher extends Person
+{
+    int salary;
+    String subject;
+    void setDetails(int s , String sub)
+    {
+        salary = s ;
+        subject = sub ;
+    }
+    void display()
+    {
+        System.out.println(nameOfPerson);
+        System.out.println(salary);
+        System.out.println(subject);
+    }
+    
+}
+class CollegeStudent extends Student
+{
+    int year;
+    String major;
+    void setDetails(int y , String m)
+    {
+        year = y ; 
+        major = m ;
+    }
+    void display()
+    {
+        System.out.println(nameOfStudent);
+        System.out.println(year);
+        System.out.println(major);
+    }
+}
